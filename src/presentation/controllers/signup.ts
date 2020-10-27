923bd81f02589c4613933ef8d87a90b7f64019cf
GeovaneDaSilva <geovaneartedesign@gmail.com>
2020-10-27 11:58:14 -0400

    feat: ensure SignUpController return 400 if email no is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,26 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    
-    return {
-      statusCode: 400,
-      body: new Error('Missing params: name')
 
+    if ( !httpRequest.body.name ) {
+      
+      return {
+        statusCode: 400,
+        body : new Error('Missing params: name')
+  
+      }
+      
     }
+    if ( !httpRequest.body.email ) {
+      
+      return {
+        statusCode: 400,
+        body : new Error('Missing params: email')
+  
+      }
+      
+    }
+    
+    
   }
 }
 //
